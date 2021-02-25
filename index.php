bdac08ab07e194bf4dd8fe6cd3fe885b8592c364
GitaMulya <gitamulya2@gmail.com>
2021-02-25 23:01:30 +0800

    ResponsiveDesain Web

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="Assets/bs5/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!--icon-->
+    <link rel="stylesheet" href="Assets/icon/css/all.min.css">
+    
+
+    <link rel="stylesheet" href="Assets/css/navbar.css">
+    <link rel="stylesheet" href="Assets/css/body.css">
+    
+
+
+    <title> Cooking Day</title>
+  </head>
+  <body>
+      <!--navbar-->
+      <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top" id="navbar">
+          <div class="container">
+              <a class="navbar-brand" href="#">
+                  <img src="Media/new.png" height="55px">
+               </a> 
+               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-lable="Toggle navigation">
+                   <span class="navbar-toggler-icon"></span> 
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <div></div>  
+                    <ul class="nav nav-pills">
+                        <li class="nav-item">
+                            <a class="nav-link active bg-active link bold1" href="#">Resep&nbsp;&nbsp;</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link bold1" href="#">Video&nbsp;&nbsp;</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link bold1" href="#">Subscribe&nbsp;&nbsp;</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link bold1" href="#">Shop&nbsp;&nbsp;</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link bold1 me-5" href="#">About Us&nbsp;&nbsp;</a>
+                        </li>
+                    </ul>
+                    <a href="#" class="btn bg-custom rounded bold1 shadow">SIGN UP</a>
+                </div>
+            </div>     
+        </nav>        
+        <!--konten-->
+        <div class="container">
+            <br><br>
+            <div class="row mt-5 mb-5">
+                <div class="col-lg-12 gambar">
+                    <img src="Media/coba.png" width="100%">
+                </div>
+                <div class="col-sm-12 position-relative p-4">
+                    <div class="position-absolute top-0 end-0">
+                        <img src="Media/coba.png" class="img">
+                    </div>
+                    <h1 class="display-1 text-truncate bold1">Let's Cooking</h1>
+                    <h1 class="display-1 text-truncate bold1">Together</h1>
+                    <div class="desc mt-4">
+                        <p>Ayo tingkatkan kemampuan memasakmu bersama CookingDay. Dengan adanya CookingDay kamu tidak perlu khawatir mengenai rasa masakkan mu, disini telah disediakan berbagai resep lengkap dengan langkah-langkah memasak dan juga video tutorial untuk membantu kamu di dapur</p>
+                    </div>
+
+                    <div class="tombol mt-5">
+                        <a href="#" class="button rounded-pill shadow bold1">Mulai Memasak</a>
+                    </div>
+                    <br>
+                    <div class="flex at-5">
+                        <a href="#" class="btn rounded-circle bg-custom shadow bold1 m-2"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="btn rounded-circle bg-custom shadow bold1 m-2"><i class="fab fa-twitter"></i></a>
+                        <a href="#" class="btn rounded-circle bg-custom shadow bold1 m-2"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="btn rounded-circle bg-custom shadow bold1 m-2"><i class="fab fa-tiktok"></i></a>
+                        <a href="#" class="btn rounded-circle bg-custom shadow bold1 m-2"><i class="fab fa-youtube"></i></a>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="Assets/bs5/dist/js/bootstrao.bundle.js"></script>
+    <script src="Assets/icon/js/all.min.js"></script>
+  </body>
+</html>
